f3902fe33d3bfcdd256e8a9d3874f49b11d6233d
Inf1nity <1449896523@qq.com>
2025-08-16 00:40:44 +0800

    Add ancient manuscript recipe

--- src/main/java/btw/community/xptome/XPTomeAddon.java
+++ src/main/java/btw/community/xptome/XPTomeAddon.java
@@ -2,9 +2,7 @@ package btw.community.xptome;
 
 import btw.BTWAddon;
 import btw.AddonHandler;
-import btw.block.BTWBlocks;
 import btw.crafting.recipe.RecipeManager;
-import btw.item.BTWItems;
 import net.minecraft.src.Item;
 import net.minecraft.src.ItemStack;
 import com.inf1nlty.xptome.ExpCommand;
@@ -16,5 +14,13 @@ public class XPTomeAddon extends BTWAddon {
         AddonHandler.logMessage(getName() + " v" + getVersionString() + " Initializing...");
         XPTomeItems.registerItems();
         AddonHandler.registerCommand(new ExpCommand(), false);
+
+        RecipeManager.addShapelessRecipe(
+                new ItemStack(Item.enchantedBook, 2),
+                new Object[]{
+                        Item.enchantedBook,
+                        Item.writableBook
+                }
+        );
     }
 }
